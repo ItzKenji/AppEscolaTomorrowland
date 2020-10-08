fdba9176059f3b2d05a0938115065e7c07a760ea
Gabriel <gkpo2014@hotmail.com>
2020-10-08 15:14:01 -0300

    Ultima versão do app

--- app/src/main/java/br/usjt/ucsist/cadbasico/ui/MainActivity.java
+++ app/src/main/java/br/usjt/ucsist/cadbasico/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import br.usjt.ucsist.cadbasico.R;
 
@@ -35,4 +36,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return(super.onOptionsItemSelected(item));
     }
+
 }
